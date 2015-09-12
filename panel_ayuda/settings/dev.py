66ed2f74be79d2313f9d6265e984766e5429e414
fcrios145 <fcrios145@hotmail.com>
2015-09-12 13:56:42 -0600

    se cambia ubicacion de binario

--- panel_ayuda/settings/dev.py
+++ panel_ayuda/settings/dev.py
@@ -125,7 +125,7 @@ STATICFILES_FINDERS = (
 )
 
 PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.uglifyjs.UglifyJSCompressor'
-PIPELINE_UGLIFYJS_BINARY = '/usr/bin/uglify'
+PIPELINE_UGLIFYJS_BINARY = '/usr/bin/uglifyjs'
 
 # CSS Files.
 PIPELINE_CSS = {
